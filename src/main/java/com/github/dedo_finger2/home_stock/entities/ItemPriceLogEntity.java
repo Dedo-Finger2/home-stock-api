5f159f483fa2bf012625e9787ba604e9c59a732c
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-21 10:47:00 -0300

    feat: adds ItemPriceLogEntity

---
+++ src/main/java/com/github/dedo_finger2/home_stock/entities/ItemPriceLogEntity.java
@@ -0,0 +1,60 @@
+package com.github.dedo_finger2.home_stock.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item_price_logs")
+public class ItemPriceLogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "inventory_items_id", nullable = false)
+    private InventoryItemEntity inventoryItem;
+
+    @Column(name = "unit_price", nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public ItemPriceLogEntity() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public InventoryItemEntity getInventoryItem() {
+        return inventoryItem;
+    }
+
+    public void setInventoryItem(InventoryItemEntity inventoryItem) {
+        this.inventoryItem = inventoryItem;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
