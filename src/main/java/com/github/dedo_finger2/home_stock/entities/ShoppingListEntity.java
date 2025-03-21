34288ec6bddabd380d818f66d565c6d659342b84
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-21 10:01:05 -0300

    feat: adds ShoppingListEntity

---
+++ src/main/java/com/github/dedo_finger2/home_stock/entities/ShoppingListEntity.java
@@ -0,0 +1,53 @@
+package com.github.dedo_finger2.home_stock.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "shopping_lists")
+public class ShoppingListEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public ShoppingListEntity() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public ShoppingListEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
